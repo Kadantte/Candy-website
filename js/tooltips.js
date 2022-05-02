0f0c96f7751bc55e281d53d621e2286e3ad0c84d
Jae Jin <minato.subs@gmail.com>
2022-05-02 16:12:46 +0000

    Big update :cake:

---
+++ js/tooltips.js
@@ -0,0 +1,62 @@
+tippy("#statusIcon", {
+  allowHTML: true,
+  content: "Loading...",
+  placement: "right-end",
+  arrow: false,
+});
+
+tippy("#themeButton", {
+  content: "Switch theme",
+  placement: "left",
+});
+
+tippy("#email", {
+  content: "Click to email me",
+  placement: "right",
+  arrow: false,
+});
+
+tippy("#discord", {
+  content: "Usui#0256",
+  placement: "bottom",
+});
+
+tippy("#twitter", {
+  content: "@kadantte",
+  placement: "bottom",
+});
+
+tippy("#github", {
+  content: "kadantte",
+  placement: "bottom",
+});
+
+/* tippy("#linkedin", {
+  content: "Eray Can Ozdemir",
+  placement: "bottom",
+});
+
+tippy("#medium", {
+  content: "Eray C.",
+  placement: "bottom",
+}); */
+
+tippy("#instagram", {
+  content: "@kadanttes",
+  placement: "bottom",
+});
+
+tippy("#french", {
+  content: "French (native)",
+  placement: "bottom",
+});
+
+tippy("#english", {
+  content: "English (B1)",
+  placement: "bottom",
+});
+
+tippy("#turkish", {
+  content: "Turkish (native)",
+  placement: "bottom",
+});
